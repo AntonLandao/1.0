49395d21c727f84da30bd52946755f4bde3135cc
AntonLandao <oriverhollyn@gmail.com>
2021-04-22 23:10:53 +0500

    dupa

---
+++ 1.4/1.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int a,b,c,k;
+	cin>>a>>b>>c>>k;
+	if (a%k==0)
+		cout<<"k divisor a"<<endl;
+	if (b%k==0)
+		cout<<"k divisor b"<<endl;
+	if (c%k==0)
+		cout<<"k divisor c"<<endl;
+	return 0;
+}
