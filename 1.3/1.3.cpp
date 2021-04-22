f19d0898ce6d082f0fc0f6b7b596a7df79b0f821
AntonLandao <oriverhollyn@gmail.com>
2021-04-22 21:06:19 +0500

    jus

---
+++ 1.3/1.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x,y;
+	cout<<"Enter x:";
+	cin>>x;
+	cout<<"Enter y:";
+	cin>>y;
+
+	{
+		if (x>0&&y>0&&x*x+y*y<=36&&y>-x+3)
+			cout<<"true";
+		else
+			cout<<"false";
+	}
+
+	return 0;
+}
