e6dcd2689cdc3f4e91c6ef634f4bbe671b222b62
AntonLandao <oriverhollyn@gmail.com>
2021-04-22 20:28:07 +0500

    kokot

---
+++ 1.0/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double x,y,res;
+	cout<<"x=";
+	cin>>x;
+	cout<<"y=";
+	cin>>y;
+	res=pow((x+1)/(x-1),x)+18*x*y*y;
+	cout<<"res="<<res<<"\n";
+	cin.get();
+	return 0;
+}
